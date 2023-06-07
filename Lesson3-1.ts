c3bd86a44df2232697ea7939cb7a05d270e748fe
Gunsnfnr <126002661+Gunsnfnr@users.noreply.github.com>
2023-06-07 14:07:25 +0300

    Update Lesson3-1.ts

--- Lesson3-1.ts
+++ Lesson3-1.ts
@@ -2,57 +2,61 @@ interface User {
   name: string;
   age: number;
   group: string;
+  type: string;
 };
 
 interface Admin {
   name: string;
   age: number;
   role: string;
+  type: string;
 }
 
-type Person = User | Admin;
-
-const persons: Person[] = [
+const persons: (User | Admin)[] = [
   {
     name: 'Иван Петров',
     age: 27,
     group: 'SEO-специалист',
+    type: 'user',
   },
   {
     name: 'Марат Aляуддинов',
     age: 20,
     group: 'Музыкант',
+    type: 'user',
   },
   {
     name: 'Ольга Николаева',
     age: 35,
     group: 'Семья',
+    type: 'user',
   },
   {
     name: 'Игорь Иванов',
     age: 40,
     group: 'Друзья',
+    type: 'user',
   },
   {
     name: 'Марат Нигматуллин',
     age: 41,
     group: 'Друзья',
+    type: 'user',
   },
   {
     name: 'Дима Билан',
     age: 45,
     role: 'Администратор',
+    type: 'admin',
   }
 ];
 
-const logPerson = (person: Person) => {
-  let adminPerson = person as Admin;
-  let userPerson = person as User;
+const logPerson = (person: any) => {
   let information: string;
-  if (adminPerson.role) {
-    information = adminPerson.role;
+  if (person.type === 'admin') {
+    information = person.role;
   } else {
-    information = userPerson.group;
+    information = person.group;
   }
   console.log(`${person.name}, ${person.age}, ${information}`);
 };
