729b7db25725777a7c8aa98376428f0aa76d330f
Gunsnfnr <126002661+Gunsnfnr@users.noreply.github.com>
2023-06-07 18:59:10 +0300

    Update Lesson3-1.ts

--- Lesson3-1.ts
+++ Lesson3-1.ts
@@ -1,51 +1,66 @@
-"use strict";
-;
-const persons = [
-    {
-        name: 'Иван Петров',
-        age: 27,
-        group: 'SEO-специалист',
-        type: 'user',
-    },
-    {
-        name: 'Марат Aляуддинов',
-        age: 20,
-        group: 'Музыкант',
-        type: 'user',
-    },
-    {
-        name: 'Ольга Николаева',
-        age: 35,
-        group: 'Семья',
-        type: 'user',
-    },
-    {
-        name: 'Игорь Иванов',
-        age: 40,
-        group: 'Друзья',
-        type: 'user',
-    },
-    {
-        name: 'Марат Нигматуллин',
-        age: 41,
-        group: 'Друзья',
-        type: 'user',
-    },
-    {
-        name: 'Дима Билан',
-        age: 45,
-        role: 'Администратор',
-        type: 'admin',
-    }
-];
-const logPerson = (person) => {
-    let information;
-    if ('role' in person) {
-        information = person.role;
-    }
-    else {
-        information = person.group;
-    }
-    console.log(`${person.name}, ${person.age}, ${information}`);
+interface User {
+  name: string;
+  age: number;
+  group: string;
+  type: string;
 };
+
+interface Admin {
+  name: string;
+  age: number;
+  role: string;
+  type: string;
+}
+
+type Person = User | Admin;
+
+const persons: Person[] = [
+  {
+    name: 'Иван Петров',
+    age: 27,
+    group: 'SEO-специалист',
+    type: 'user',
+  },
+  {
+    name: 'Марат Aляуддинов',
+    age: 20,
+    group: 'Музыкант',
+    type: 'user',
+  },
+  {
+    name: 'Ольга Николаева',
+    age: 35,
+    group: 'Семья',
+    type: 'user',
+  },
+  {
+    name: 'Игорь Иванов',
+    age: 40,
+    group: 'Друзья',
+    type: 'user',
+  },
+  {
+    name: 'Марат Нигматуллин',
+    age: 41,
+    group: 'Друзья',
+    type: 'user',
+  },
+  {
+    name: 'Дима Билан',
+    age: 45,
+    role: 'Администратор',
+    type: 'admin',
+  }
+];
+
+const logPerson = (person: Person) => {
+  let information: string;
+  if ('role' in person) {
+    information = person.role;
+  } else {
+    information = person.group;
+  }
+  console.log(`${person.name}, ${person.age}, ${information}`);
+};
+
 persons.forEach(logPerson);
